ff35f55d5f751662675267a3fed872cd37391d53
brian bates <batesbj24@gmail.com>
2020-11-28 15:23:26 -0500

    calculate average

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,11 +1,12 @@
 import csv 
-
+from statistics import mean
 Total = 0.0
 Averagechange = 0.0
 Maxprofit = []
 Minprofit = []
 Profitloss = []
-with open('Resources/budget_data.csv') as csv_file:
+
+with open('Pybank/Resources/budget_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -15,8 +16,9 @@ with open('Resources/budget_data.csv') as csv_file:
         else:
             print(f'\t{row[0]} {row[1]}')
             Total = Total + int(row[1])
-            Profitloss.append(row[1])
+            Profitloss.append(int(row[1]))
             line_count += 1
     print(f'Processed {line_count} lines.')
     print (Total)
-    print (Profitloss)
+
+    print (mean(Profitloss))
