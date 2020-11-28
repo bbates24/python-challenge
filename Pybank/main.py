d2fda2633e60b6c13fe6555238c121aa7518334c
brian bates <batesbj24@gmail.com>
2020-11-28 15:02:34 -0500

    calculate profitloss

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,4 +1,10 @@
 import csv 
+
+Total = 0.0
+Averagechange = 0.0
+Maxprofit = []
+Minprofit = []
+Profitloss = []
 with open('Resources/budget_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -8,5 +14,9 @@ with open('Resources/budget_data.csv') as csv_file:
             line_count += 1
         else:
             print(f'\t{row[0]} {row[1]}')
+            Total = Total + int(row[1])
+            Profitloss.append(row[1])
             line_count += 1
     print(f'Processed {line_count} lines.')
+    print (Total)
+    print (Profitloss)
